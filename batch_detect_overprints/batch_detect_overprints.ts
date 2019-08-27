7ec5da712ccac60d94caa32a7fd97752e535dd62
lumenn <silmelumenn@gmail.com>
2019-08-27 09:49:44 +0200

    Added function to check types in Arrary of Files|Folders

--- batch_detect_overprints/batch_detect_overprints.ts
+++ batch_detect_overprints/batch_detect_overprints.ts
@@ -1,9 +1,6 @@
 // Variables to adjust the script:
 const SELECT_DIALOG_FOLDER_START_PATH: string = 'C:\\Maks-Pliki\\Skrypty\\Kod\\Illustrator\\adobe-forums\\batch_detect_overprints\\Files';
 
-function isArrayOfFiles(item: (File | Folder)[]): item is File[] {
-  displayMessage(item.type);
-}
 
 function displayMessage(text: string): void {
   const messageWindow = new Window('dialog', undefined, undefined, {
@@ -12,6 +9,23 @@ function displayMessage(text: string): void {
   messageWindow.add('statictext', undefined, text);
   messageWindow.add('button', undefined, 'OK');
   messageWindow.show();
+}
+
+function isArrayOfFiles(items: (File | Folder)[]): items is File[] {
+  let folderCounter: number = 0;
+
+  for (let i = 0; i < items.length; i += 1) {
+    const item: File|Folder = items[i];
+    if (item instanceof Folder) {
+      folderCounter += 1;
+    }
+  }
+
+  if (folderCounter !== 0) {
+    return null;
+  }
+
+  return items as File[] !== undefined;
 }
 
 function getFolderURI(): string {
@@ -27,21 +41,18 @@ function getFolderURI(): string {
   return folderURI;
 }
 
-function processFiles(files: File[]): void {
+// function processFiles(files: File[]): void {
 
-}
+// }
 
 function getFiles(folderURI: string): File[] {
   const folder: Folder = new Folder(folderURI);
   const files: (File | Folder)[] = folder.getFiles();
 
   if (isArrayOfFiles(files)) {
-    for (let i = 0; i < files.length; i = i + 1) {
-      const file: File = files[i];
-      displayMessage(file.name);
-    }
-    return files;
+    displayMessage('Files');
   }
+  
 }
 
 function main(): void {
