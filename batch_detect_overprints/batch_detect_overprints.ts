0de26764f9a089ba26edf6006bfcad8da52bc13e
lumenn <silmelumenn@gmail.com>
2019-08-27 10:31:44 +0200

    Added comments, created filter function for Array of Files | Folders

--- batch_detect_overprints/batch_detect_overprints.ts
+++ batch_detect_overprints/batch_detect_overprints.ts
@@ -1,6 +1,10 @@
 // Variables to adjust the script:
 const SELECT_DIALOG_FOLDER_START_PATH: string = 'C:\\Maks-Pliki\\Skrypty\\Kod\\Illustrator\\adobe-forums\\batch_detect_overprints\\Files';
 
+/**
+ * Displays message to the user
+ * @param text Text to display
+ */
 
 function displayMessage(text: string): void {
   const messageWindow = new Window('dialog', undefined, undefined, {
@@ -10,6 +14,11 @@ function displayMessage(text: string): void {
   messageWindow.add('button', undefined, 'OK');
   messageWindow.show();
 }
+
+/**
+ * Check if array contains only Files
+ * @param items Array to check
+ */
 
 function isArrayOfFiles(items: (File | Folder)[]): items is File[] {
   let folderCounter: number = 0;
@@ -28,6 +37,10 @@ function isArrayOfFiles(items: (File | Folder)[]): items is File[] {
   return items as File[] !== undefined;
 }
 
+/**
+ * Returns URI for folder chosed in select dialog
+ */
+
 function getFolderURI(): string {
   const tempFolder = new Folder(SELECT_DIALOG_FOLDER_START_PATH);
   let folderURI: string;
@@ -41,18 +54,37 @@ function getFolderURI(): string {
   return folderURI;
 }
 
-// function processFiles(files: File[]): void {
+/**
+ * Removes folders from array
+ * @param array Array of to filter
+ */
 
-// }
+function filterArrayForFiles(array: (File | Folder)[]): File[] {
+  const newArray: File[] = [];
+
+  for (let i = 0; i < array.length; i += 1) {
+    const element: File | Folder = array[i];
+
+    if (element instanceof File) {
+      newArray.push(element);
+    }
+  }
+  return newArray;
+}
+
+/**
+ * Gets files from specified folder
+ * @param folderURI Uri adress for folder containing files;
+ */
 
 function getFiles(folderURI: string): File[] {
   const folder: Folder = new Folder(folderURI);
-  const files: (File | Folder)[] = folder.getFiles();
+  const files: (File | Folder)[] = folder.getFiles('*.ai');
 
   if (isArrayOfFiles(files)) {
-    displayMessage('Files');
+    return files;
   }
-  
+  return filterArrayForFiles(files);
 }
 
 function main(): void {
@@ -60,7 +92,8 @@ function main(): void {
 
   if (folderURI) {
     displayMessage('Works');
-    getFiles(folderURI);
+    const filesToCheck: File[] = getFiles(folderURI);
+    displayMessage(filesToCheck.length);
   } else {
     displayMessage('File was not chosen, aborting script');
   }
