9ba29ff4e3261446f90f86f71a3e540000b68f20
lumenn <silmelumenn@gmail.com>
2022-03-13 17:10:39 +0100

    Adjusted tsconfig ant eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,24 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  // extends: ['@airbnb-typescript'],
-  extends: ['airbnb-typescript/base'],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    project: './tsconfig.json',
+  },
+  overrides: [
+    {
+      files: ['*.ts'], 
+      extends: ['airbnb-typescript/base'],
+    }
+  ],
+  plugins: ['@typescript-eslint', 'import'],
   rules: {
     'no-undef': 'off',
     'valid-typeof': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages', 
+      {
+        'ts': 'never'
+      },
+    ]
   }
 }
